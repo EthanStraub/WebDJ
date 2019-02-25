b5a4775d4b6f7221fbebd56af5b0242ac5677c68
EthanStraub <ethan.straub@gmail.com>
2019-02-25 10:11:19 -0600

    Removed administrator access to 'follow' feature.

--- WebDjProject/Controllers/UsersController.cs
+++ WebDjProject/Controllers/UsersController.cs
@@ -92,7 +92,7 @@ namespace WebDjProject.Controllers
             client.Timeout = 10000;
             client.DeliveryMethod = SmtpDeliveryMethod.Network;
             client.UseDefaultCredentials = false;
-            client.Credentials = new System.Net.NetworkCredential("biblebot99@gmail.com", "Carbuncle#9");
+            client.Credentials = new System.Net.NetworkCredential("webdjmessaging@gmail.com", "Carbuncle#9");
 
             MailMessage mm = new MailMessage(address, address, "WebDJ -- Premium Account Recommendations", "Hello " + name + "! Based on the playlist '" + playlistName + "' you made using our website, here are some songs we think you might like. \n https://open.spotify.com/track/" + Songlist[0] + " \n https://open.spotify.com/track/" + Songlist[1] + " \n https://open.spotify.com/track/" + Songlist[2] + " \n https://open.spotify.com/track/" + Songlist[3] + " \n https://open.spotify.com/track/" + Songlist[4]);
             mm.BodyEncoding = UTF8Encoding.UTF8;
